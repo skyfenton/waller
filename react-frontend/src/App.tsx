838286f65fdadf214a94251711b71f7be3575e67
Schuyler Fenton <dev@skyfenton.com>
2024-05-25 02:13:31 +0000

    Add TODO reminder

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useUploadFile } from '@/hooks/use-upload-file';
 import { SingleFileUploader } from './components/file-uploader';
 
 function App() {
+  // TODO refactor useUploadFile to talk to backend (and then replace temp
+  // values in component)
   const { uploadFile, progress, uploadedFiles, isUploading } = useUploadFile('imageUploader', {
     defaultUploadedFiles: []
   });
@@ -10,7 +12,7 @@ function App() {
     <div className=" container flex min-h-screen flex-col place-content-center ">
       <SingleFileUploader
         maxSize={2 * 1024 * 1024}
-        progress={progress}
+        progress={50}
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         onUpload={() => new Promise(() => {})}
         disabled={isUploading}
