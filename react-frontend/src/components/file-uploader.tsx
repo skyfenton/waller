a1740fe579bcf73cb2c5bdf8fdaeb619961e3115
Schuyler Fenton <dev@skyfenton.com>
2024-06-01 07:50:19 +0000

    Transform dropzone to filecard on upload

--- react-frontend/src/components/file-uploader.tsx
+++ react-frontend/src/components/file-uploader.tsx
@@ -156,69 +156,58 @@ export function SingleFileUploader(props: FileUploaderProps) {
 
   return (
     <div className="relative flex flex-col gap-6 overflow-hidden">
-      <Dropzone
-        onDrop={onDrop}
-        accept={accept}
-        maxSize={maxSize}
-        maxFiles={1}
-        multiple={false}
-        disabled={isDisabled}
-      >
-        {({ getRootProps, getInputProps, isDragActive }) => (
-          <div
-            {...getRootProps()}
-            className={cn(
-              'group relative grid h-52 w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed border-muted-foreground/25 px-5 py-2.5 text-center transition hover:bg-muted/25',
-              'ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
-              isDragActive && 'border-muted-foreground/50',
-              isDisabled && 'pointer-events-none opacity-60',
-              className
-            )}
-            {...dropzoneProps}
-          >
-            <input {...getInputProps()} />
-            <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
-              <div className="rounded-full border border-dashed p-3">
-                <UploadIcon
-                  className="text-muted-foreground size-7"
-                  aria-hidden="true"
-                />
-              </div>
-              {isDragActive ? (
-                <p className="text-muted-foreground font-medium">
-                  Drop the file here
-                </p>
-              ) : (
-                <div className="space-y-px">
-                  <p className="text-muted-foreground font-medium">
-                    Drag {`'n`} drop an image here, or click to select an image
-                  </p>
-                  <p className="text-muted-foreground/70 text-sm">
-                    {`You can upload an image with a max size of 
-                    ${formatBytes(maxSize)}`}
-                  </p>
-                </div>
-              )}
-            </div>
-          </div>
-        )}
-      </Dropzone>
       {file ? (
-        <ScrollArea className="h-fit w-full px-3">
-          <div className="max-h-48 space-y-4">
-            {
-              <FileCard
-                // key={index}
-                file={file}
-                onRemove={() => {
-                  onRemove();
-                }}
-                progress={progress}
-              />
-            }
-          </div>
-        </ScrollArea>
-      ) : null}
+        <FileCard file={file} onRemove={onRemove} progress={progress} />
+      ) : (
+        <Dropzone
+          onDrop={onDrop}
+          accept={accept}
+          maxSize={maxSize}
+          maxFiles={1}
+          multiple={false}
+          disabled={isDisabled}
+        >
+          {({ getRootProps, getInputProps, isDragActive }) => (
+            <div
+              {...getRootProps()}
+              className={cn(
+                'group relative grid h-52 w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed border-muted-foreground/25 px-5 py-2.5 text-center transition hover:bg-muted/25',
+                'ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+                isDragActive && 'border-muted-foreground/50',
+                isDisabled && 'pointer-events-none opacity-60',
+                className
+              )}
+              {...dropzoneProps}
+            >
+              <input {...getInputProps()} />
+              <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
+                <div className="rounded-full border border-dashed p-3">
+                  <UploadIcon
+                    className="text-muted-foreground size-7"
+                    aria-hidden="true"
+                  />
+                </div>
+                {isDragActive ? (
+                  <p className="text-muted-foreground font-medium">
+                    Drop the file here
+                  </p>
+                ) : (
+                  <div className="space-y-px">
+                    <p className="text-muted-foreground font-medium">
+                      Drag {`'n`} drop an image here, or click to select an
+                      image
+                    </p>
+                    <p className="text-muted-foreground/70 text-sm">
+                      {`You can upload an image with a max size of 
+                    ${formatBytes(maxSize)}`}
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+        </Dropzone>
+      )}
     </div>
   );
 }
