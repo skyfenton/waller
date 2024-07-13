2cf362997983aa6fdd7dfce6b2968ee8dc8d14a4
Schuyler Fenton <dev@skyfenton.com>
2024-07-13 02:04:31 +0000

    Add button to abandon editing image

--- react-frontend/src/pages/edit.tsx
+++ react-frontend/src/pages/edit.tsx
@@ -1,7 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Link, useParams } from 'react-router-dom';
 // TODO: Come up with better name than Edit/Image Page?
 
 export default function ImagePage() {
+  const { imgId } = useParams();
+
   return (
-    <><p>Edit: Not yet implemented</p></>
-  )
+    <div className="container min-h-screen">
+      <Link to="/">
+        <Button variant="destructive">Back to Upload</Button>
+      </Link>
+    </div>
+  );
 }
