fe1f0b1a895a3533ae7ebb8587aa2c864f11b230
Schuyler Fenton <dev@skyfenton.com>
2024-09-28 06:11:30 +0000

    Convert blob to HTMLImageElement and display on pixijs app

--- react-frontend/src/pages/edit.tsx
+++ react-frontend/src/pages/edit.tsx
@@ -1,32 +1,43 @@
 import { Button } from '@/components/ui/button';
 import { isFileWithPreview } from '@/lib/utils';
 import { WallerJob } from '@/types';
+
+import { Stage, Container, Sprite } from '@pixi/react';
+import { useEffect, useRef } from 'react';
 // TODO: Come up with better name than Edit/Image Page?
 
 export default function EditPage(props: {
   job: WallerJob;
   setJob: (job: WallerJob | undefined) => void;
 }) {
-  // TODO: Get image mask before editing
+  const previewDiv = useRef<HTMLDivElement>(null);
   const mask =
     (import.meta.env.VITE_SERVER_URL as string) + `/images/${props.job.id}.png`;
-  // const mask = (import.meta.env.VITE_SERVER_URL as string) + '/images/star.png';
+
+  const getImage = (src: File) => {
+    if (isFileWithPreview(src)) {
+      const img = new Image();
+      img.src = src.preview;
+      return img;
+    }
+  };
 
   return (
     <div className="container flex min-h-screen flex-col justify-evenly">
-      <div id="preview">
+      <div ref={previewDiv}>
         {isFileWithPreview(props.job.image) ? (
-          <img
-            src={props.job.image.preview}
-            alt={props.job.image.name}
-            loading="lazy"
-            className="w-auto shrink-0 object-cover"
-            style={{
-              maskImage: `url(${mask})`,
-              WebkitMaskImage: `url(${mask})`,
-              maskSize: 'cover'
+          <Stage
+            width={1280}
+            height={720}
+            options={{
+              resizeTo: previewDiv.current as HTMLElement
+              // backgroundAlpha: 0
             }}
-          />
+          >
+            <Container>
+              <Sprite image={getImage(props.job.image)} />
+            </Container>
+          </Stage>
         ) : (
           <h3>Loading picture...</h3>
         )}
@@ -42,4 +53,23 @@ export default function EditPage(props: {
       </Button>
     </div>
   );
+
+  // return (
+  //   <Stage options={{ background: 0xffffff }}>
+  //     <Sprite
+  //       image="https://pixijs.io/pixi-react/img/bunny.png"
+  //       x={400}
+  //       y={270}
+  //       anchor={{ x: 0.5, y: 0.5 }}
+  //     />
+
+  //     <Container x={400} y={330}>
+  //       <Text
+  //         text="Hello World"
+  //         anchor={{ x: 0.5, y: 0.5 }}
+  //         filters={[new BlurFilter()]}
+  //       />
+  //     </Container>
+  //   </Stage>
+  // )
 }
