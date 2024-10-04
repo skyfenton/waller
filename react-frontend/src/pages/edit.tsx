7d175ec4f14610ad39ab11873b972e8716310198
Schuyler Fenton <dev@skyfenton.com>
2024-10-04 18:48:36 +0000

    Resize stage to div on animation frame

--- react-frontend/src/pages/edit.tsx
+++ react-frontend/src/pages/edit.tsx
@@ -3,14 +3,48 @@ import { isFileWithPreview } from '@/lib/utils';
 import { WallerJob } from '@/types';
 
 import { Stage, Container, Sprite } from '@pixi/react';
-import { useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
+
+const useResize = (elementRef: React.RefObject<HTMLElement>) => {
+  const [size, setSize] = useState([
+    elementRef.current?.clientWidth ?? 1920,
+    elementRef.current?.clientHeight ?? 1080
+  ]);
+
+  useEffect(() => {
+    const onResize = () => {
+      requestAnimationFrame(() => {
+        if (elementRef.current) {
+          setSize([
+            elementRef.current.clientWidth,
+            elementRef.current.clientHeight
+          ]);
+        }
+      });
+    };
+
+    onResize();
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  return size;
+};
 // TODO: Come up with better name than Edit/Image Page?
 
 export default function EditPage(props: {
   job: WallerJob;
   setJob: (job: WallerJob | undefined) => void;
 }) {
-  const previewDiv = useRef<HTMLDivElement>(null);
+  // const canvasRatio = 3 / 2;
+  // const [canvasSize, setCanvasSize] = useState({ width: 100, height: 100 });
+
+  const previewArea = useRef<HTMLDivElement>(null);
+  const [width, height] = useResize(previewArea);
+
   const mask =
     (import.meta.env.VITE_SERVER_URL as string) + `/images/${props.job.id}.png`;
 
@@ -22,16 +56,19 @@ export default function EditPage(props: {
     }
   };
 
+  // useEffect(() => {
+  //   console.log(previewArea.current);
+  // });
+
   return (
     <div className="container flex min-h-screen flex-col justify-evenly">
-      <div ref={previewDiv}>
+      <div ref={previewArea}>
         {isFileWithPreview(props.job.image) ? (
           <Stage
-            width={1920}
-            height={1200}
-            style={{ width: '100%', height: '100%' }}
+            width={width}
+            height={height}
             options={{
-              resizeTo: previewDiv.current as HTMLElement
+              autoDensity: true
               // backgroundAlpha: 0
             }}
           >
