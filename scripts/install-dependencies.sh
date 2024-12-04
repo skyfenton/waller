c413d91b9a2c7eb135158fcca5a8191b16299ef8
Schuyler Fenton <dev@skyfenton.com>
2024-12-04 21:49:41 +0000

    Fix cd command to work with directories using spaces

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,14 +1,15 @@
 cd $(dirname $BASH_SOURCE)
 cd ..
 
-PROJ_DIR=$(pwd)
+PROJ_DIR=$(printf "%q\n" "$(pwd)")
+# PROJ_DIR="/workspaces/'21.13 waller'"
 
 echo "Project Directory: $PROJ_DIR"
 
-cd ${PROJ_DIR}/python-backend
+eval cd "$PROJ_DIR/python-backend/"
 
 pip install -r requirements.txt
 
-cd ${PROJ_DIR}/react-frontend
+eval cd "$PROJ_DIR/react-frontend/"
 
 pnpm install
