5ca8ce759ac2cd68861d3a07ec6db61f33e5b59a
Schuyler Fenton <dev@skyfenton.com>
2025-01-04 23:10:15 +0000

    Add api gateway integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,19 +16,75 @@ locals {
   lambda_folder_path = abspath(var.path_to_lambda_folder)
 }
 
-module "lambda_function" {
+module "test_lambda" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "~> 7.0"
 
   function_name = "test-lambda"
-  # description   = "My awesome lambda function"
+  description   = "Lambda for testing waller API"
   handler = "index.lambda_handler"
   runtime = "python3.12"
+  publish = true
 
   source_path = "${local.lambda_folder_path}/test"
+}
 
-  tags = {
-    name = "test-lambda"
+module "dev_alias" {
+  source  = "terraform-aws-modules/lambda/aws//modules/alias"
+  version = "~> 7.0"
+
+  name          = "dev"
+
+  refresh_alias = false
+
+  function_name = module.test_lambda.lambda_function_name
+  function_version = "$LATEST"
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service    = "apigateway"
+      source_arn = "${module.api_gateway.api_execution_arn}/*/*/test"
+    }
+  }
+}
+
+# module "upload_lambda" {
+#   source  = "terraform-aws-modules/lambda/aws"
+#   version = local.lambda_module_version
+
+#   function_name = "upload-lambda"
+#   handler = "index.lambda_handler"
+#   runtime = "python3.12"
+
+#   source_path = "${local.lambda_folder_path}/upload"
+
+#   tags = {
+#     name="upload-lambda"
+#   }
+# }
+
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
+  version = "~> 5.0"
+
+  name          = "waller-api-gateway"
+  description   = "HTTP API Gateway for Waller"
+  protocol_type = "HTTP"
+
+  # domain_name = var.api_domain_name
+  # domain_name_certificate_arn = var.domain_cert_arn
+  create_domain_name = false
+  create_certificate = false
+  create_domain_records = false
+
+  routes = {
+    "POST /test" = {
+      integration = {
+        uri                    = module.dev_alias.lambda_alias_arn
+        payload_format_version = "2.0"
+        timeout_milliseconds   = 12000
+      }
+    }
   }
 }
 
