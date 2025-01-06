a006b33f6c49f698837df3fe900e2ad0216aef8d
Schuyler Fenton <dev@skyfenton.com>
2025-01-06 17:50:06 +0000

    Update test function for testing uploads

--- python-backend/lambda/test/index.py
+++ python-backend/lambda/test/index.py
@@ -1,7 +1,7 @@
 def lambda_handler(event, context):
     print("Hello, Lambda!")
-    print(event)
-    print(context)
+    print(event['headers']['content-type'])
+    print(event['body'][:128]+f"... ({len(event['body'])})")
     return {
         'statusCode': 200,
         'body': 'Lambda is working!'
