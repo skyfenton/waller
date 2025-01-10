fc6bdeea8b5be04cb6d58f5b3dedce768949572f
Schuyler Fenton <dev@skyfenton.com>
2025-01-10 17:24:01 +0000

    Schedule lambda function to remove objects after 30 minutes

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,9 +36,13 @@ module "test_lambda" {
   }
 }
 
+#############################################
+# S3 BUCKET AND EXPIRATION CRON
+#############################################
+
 module "waller_image_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.3.0"
+  version = "~> 4.3"
 
   bucket        = "waller-images"
   force_destroy = true
@@ -48,6 +52,80 @@ module "waller_image_bucket" {
 
   # lambda_function = 
 }
+
+data "aws_iam_policy_document" "expire_objects_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::waller-images"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:DeleteObject"
+    ]
+    resources = [
+      "arn:aws:s3:::waller-images/*"
+    ]
+  }
+}
+
+module "expire_objects_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 7.0"
+
+  function_name = "expire-waller-images"
+  description   = "Lambda for cron job to expire waller images"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.12"
+  publish       = true
+
+  source_path = "${local.lambda_folder_path}/expire_items"
+
+  trusted_entities = ["scheduler.amazonaws.com"]
+
+  allowed_triggers = {
+    ScanAmiRule = {
+      principal  = "scheduler.amazonaws.com"
+      source_arn = module.eventbridge.eventbridge_schedule_arns["lambda-cron"]
+    }
+  }
+
+  attach_policy_json = true
+  policy_json        = data.aws_iam_policy_document.expire_objects_policy.json
+}
+
+module "eventbridge" {
+  source = "terraform-aws-modules/eventbridge/aws"
+
+  create_bus = false
+
+  attach_lambda_policy = true
+  lambda_target_arns   = [module.expire_objects_lambda.lambda_function_arn]
+
+  schedules = {
+    lambda-cron = {
+      description         = "Trigger for a Lambda"
+      schedule_expression = "rate(30 minutes)"
+      timezone            = "Europe/London"
+      arn                 = module.expire_objects_lambda.lambda_function_arn
+      input               = jsonencode({ "job" : "cron-by-rate" })
+      retry_policy = {
+        maximum_retry_attempts       = 3
+        maximum_event_age_in_seconds = 300
+      }
+    }
+  }
+}
+
+
+#############################################
+# API GATEWAY AND ROUTING
+#############################################
 
 data "aws_iam_policy_document" "put_object_policy" {
   statement {
