b96f52f6aabe34ef85e98b986b08d4f28a6ce876
Schuyler Fenton <dev@skyfenton.com>
2025-01-13 19:27:31 +0000

    Remove unused variables and comments

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,16 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-    # archive = {
-    #   source  = "hashicorp/archive"
-    #   version = "~> 2.0"
-    # }
-
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    
+
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,32 @@
 variable "aws_config" {
-    type = object({
-        shared_config_files      = list(string)
-        shared_credentials_files = list(string)
-        profile                  = string
-    })
+  type = object({
+    shared_config_files      = list(string)
+    shared_credentials_files = list(string)
+    profile                  = string
+  })
 
-    default = {
-        shared_config_files      = []
-        shared_credentials_files = []
-        profile                  = "default"
-    }
+  default = {
+    shared_config_files      = []
+    shared_credentials_files = []
+    profile                  = "default"
+  }
 }
 
 variable "cloudflare_api_token" {
-    type = string
-}
-
-variable "launch_region" {
-    type = string
+  type = string
 }
 
 variable "path_to_lambda_folder" {
-    type = string
+  type = string
 }
 
 variable "api_address" {
-    type = object({
-      domain = string
-      subdomain = string
-    })
+  type = object({
+    subdomain = string
+    domain    = string
+  })
 }
 
 variable "domain_cert_arn" {
-    type = string
+  type = string
 }
