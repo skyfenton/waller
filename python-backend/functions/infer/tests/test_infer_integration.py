9264c9a5194fb5df2e3cab5d9a807d97a437d7f0
Schuyler Fenton <dev@skyfenton.com>
2025-01-27 20:03:52 +0000

    Re-add integration test for segmentation model

---
+++ python-backend/functions/infer/tests/test_infer_integration.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import pytest
+import boto3
+import os
+from moto import mock_aws
+
+os.environ["MODEL_PATH"] = "facebook/mask2former-swin-large-ade-semantic"
+from functions.infer.src.index import lambda_handler
+
+INPUT_PATH = Path(__file__).parent / "tmp" / "test.jpg"
+OUTPUT_PATH = Path(__file__).parent / "tmp" / "output.png"
+
+
+@pytest.fixture(autouse=True)
+def reset():
+    OUTPUT_PATH.unlink(missing_ok=True)
+    yield
+
+
+@mock_aws
+def test_inference_integration():
+    conn = boto3.resource("s3", region_name="us-east-1")
+    # We need to create the bucket since this is all in Moto's 'virtual' AWS account
+    conn.create_bucket(Bucket="waller-inference")
+    # Upload a test image to mock bucket
+    with open(INPUT_PATH, "rb") as input_img:
+        conn.Object("waller-inference", "queued/test").put(Body=input_img.read())
+    print(list(conn.Bucket("waller-inference").objects.all()))
+    lambda_handler(
+        {
+            "Records": [
+                {
+                    "eventVersion": "2.1",
+                    "s3": {
+                        "bucket": {"name": "waller-inference"},
+                        "object": {"key": "queued/test"},
+                    },
+                }
+            ]
+        },
+        None,
+    )
+    assert conn.Object("waller-inference", "processed/test").get()["Body"]
+    # Download the processed image
+    with open(OUTPUT_PATH, "wb") as output_img:
+        output_img.write(
+            conn.Object("waller-inference", "processed/test").get()["Body"].read()
+        )
