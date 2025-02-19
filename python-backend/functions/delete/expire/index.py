acde0ca1ff65aa59dbaaa3c9bc01e206ead1de00
Schuyler Fenton <dev@skyfenton.com>
2025-02-19 09:38:32 +0000

    Modify code to use dt.UTC

--- python-backend/functions/delete/expire/index.py
+++ python-backend/functions/delete/expire/index.py
@@ -1,18 +1,21 @@
 import boto3
-import datetime
+import datetime as dt
 
 dynamodb = boto3.resource("dynamodb")
 s3 = boto3.client("s3")
 
 
 def lambda_handler(event, context):
-    """Deletes all objects in the waller-images bucket that are older than 30 minutes"""
+    """Deletes all objects in the waller-images bucket that are older than 30
+    minutes, triggered by a cron scheduled event on EventBridge"""
     table = dynamodb.Table("waller")
 
-    now = datetime.datetime.now()
-    delta = datetime.timedelta(minutes=30)
+    now = dt.datetime.now(dt.UTC)
+    delta = dt.timedelta(minutes=30)
     threshold = now - delta
 
+    # TODO: Group items into batches and delete prior batch
+    # Should save time and cost on reads instead of scanning all items
     old_items = dynamodb.Table("waller").scan(
         Select="ALL_ATTRIBUTES",
         FilterExpression="modified_at < :threshold AND stage = :stage",
@@ -34,7 +37,3 @@ def lambda_handler(event, context):
             ]
         },
     )
-    # response = s3.list_objects_v2(Bucket="waller-images")
-    # if "Contents" in response:
-    #     for obj in response["Contents"]:
-    #         delete_data()
